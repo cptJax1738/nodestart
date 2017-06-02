851770a82ccc1b5f33aa0a702c26d00ddbbce646
Yoh Gulam <Yoh@yMac.local>
2017-06-02 00:00:41 -0400

    init and commit

---
+++ consoleEx.js
@@ -0,0 +1,12 @@
+//	const Console = require('console').console;	
+// strange because even when I commented it out 
+
+const message = "Dude wassup";
+var changedmsg;
+
+console.log('hello world');
+console.log('Yo %s', message);
+console.error(new Error('whoops something happened'));
+
+const 	name = 'Yoh Gulam';
+console.warn('Danger ${name}! Danger!');
